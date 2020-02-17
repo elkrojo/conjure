d1289a4aaa2a49302c0e3d6d531dedc9ed7bb0a5
Ronan C <rojocas@gmail.com>
2020-02-17 12:29:08 +0000

    Added home route & environment variables

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'conjure'
-app.config["MONGO_URI"] = os.environ.get('MONGODB_NAME')
+app.config["MONGO_DBNAME"] = os.environ.get('MONGODB_NAME')
+app.config["MONGO_URI"] = os.environ.get('MONGODB_CONX')
+
+mongo = PyMongo(app)
+
+
+@app.route('/')
+@app.route('/home')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
